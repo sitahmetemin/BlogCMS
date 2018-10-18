0381a2b4f8426584842e7c6864a2b100e8da09c8
sitahmetemin <sitahmetemin@hotmail.com>
2018-10-18 16:29:43 +0300

    Controllerlar oluşturuldu ve Tema giydirilmeye başlandı

---
+++ BlogCMS.Admin/Components/MenuViewComponent.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+
+namespace BlogCMS.Admin.Components
+{
+    public class MenuViewComponent : ViewComponent
+    {
+        public ViewViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
---
+++ BlogCMS.Admin/Controllers/FileManagerController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogCMS.Admin.Controllers
+{
+    public class FileManagerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
